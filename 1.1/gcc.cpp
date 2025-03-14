772bf9a27c16e2556d5b8f1cf0730c6573a60c86
masrafi-000 <smmasrafi000@gmail.com>
2025-03-14 17:02:43 +0600

    datatypes

--- 1.1/gcc.cpp
+++ 1.1/gcc.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hey there" << endl;
+    // cout << "Hey there" << endl;
+    int items = 50;
+    double cost_per_item = 9.99;
+    double total_cost = items * cost_per_item;
+    char currency = '$';
+    char grade = 'h';
+    int value = grade;
+    cout << "Number of items: " << items << "\n";
+    cout << "Cost per item: " << cost_per_item << "" << currency << "\n";
+    cout << "Total cost = " << total_cost << "" << currency << "\n";
+    cout << sizeof(value) << endl;
     return 0;
 }
