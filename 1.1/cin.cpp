6dd16fe4a5c4ef7c25ceeeffd5aaba4d010b0594
masrafi-000 <smmasrafi000@gmail.com>
2025-03-14 19:38:08 +0600

    Updated

--- 1.1/cin.cpp
+++ 1.1/cin.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 int main()
 {
-    int age;
-    cout << "Enter the age: ";
-    cin >> age;
+    // int age;
+    // cout << "Enter the age: ";
+    // cin >> age;
 
-    cout << "Your age is : " << age << endl;
+    // cout << "Your age is : " << age << endl;
+
+    int a = 10, b = 5;
+    cout << "sum = " << (a + b) << endl;
+    cout << "difference = " << (a - b) << endl;
+    cout << "division = " << (a / b) << endl;
+    cout << "product = " << (a * b) << endl;
+    cout << "modulo = " << (a % b) << endl;
+
     return 0;
 }
