6b45f68b335a544027a8ef9809b3defd43c1f558
masrafi-000 <smmasrafi000@gmail.com>
2025-03-16 21:15:20 +0600

    loop finished

--- 1.1/pattern.cpp
+++ 1.1/pattern.cpp
@@ -104,6 +104,44 @@ int main()
     //     cout << endl;
     // }
 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*" ;
+        }
+        //spaces
+        for (int j = 0; j < (2* (n-i-1)) ; j++)
+        {
+            cout << " ";
+        }
+        
+        for (int j = 0; j < i+1; j++)
+        {
+            cout << "*";
+        }
+        
+        cout << endl;
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i ; j++)
+        {
+            cout << "*";
+        }
+        // spaces
+        for (int j = 0; j < 2*i ; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n-i ; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
     
 
     return 0;
