372769c88ddec9139f6e323b72ae37b7ccfe2f57
masrafi-000 <smmasrafi000@gmail.com>
2025-03-17 12:25:44 +0600

    function added

---
+++ 1.1/function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// int factorial(int n)
+// {
+//     int fact = 1;
+
+//     for (int i = 1; i <= n; i++) 
+//     {
+//         fact *= i;
+//     }
+//     return fact;
+// }
+
+// int nCr(int n, int r)
+// {
+//     int fact_n = factorial(n);
+//     int fact_r = factorial(r);
+//     int fact_n_r = factorial(n - r);
+//     return fact_n / (fact_r * fact_n_r);
+// }
+
+// int prime(int n) {
+//     for (int i = 2; i < n; i++) {
+//         if (n % i == 0) {
+//             cout << "Not Prime" << endl;
+//             return n;
+//         } else{
+//             cout << "Prime" << endl;
+//         }
+//     }
+//     return n;
+// }
+
+
+int main()
+{
+    
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // cout << prime(n) << endl;
+    // int n = 4, r = 2;
+    // cout << nCr(n, r) << endl;
+    return 0;
+}
