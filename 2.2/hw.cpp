0eb6c51db43de82ad31933171cee969832657df7
masrafi-000 <smmasrafi000@gmail.com>
2025-03-22 19:42:03 +0600

    array_intersection

--- 2.2/hw.cpp
+++ 2.2/hw.cpp
@@ -16,7 +16,8 @@ void sumProduct(int arr[], int n)
 
 void maxMin(int arr[], int n)
 {
-    if (n == 0) return;
+    if (n == 0)
+        return;
 
     int minIndex = 0, maxIndex = 0;
 
@@ -24,28 +25,87 @@ void maxMin(int arr[], int n)
     {
         if (arr[i] < arr[minIndex])
         {
-            minIndex =i;
+            minIndex = i;
         }
         if (arr[i] > arr[maxIndex])
         {
-            maxIndex=i;
-        }        
+            maxIndex = i;
+        }
     }
-    
+
     swap(arr[minIndex], arr[maxIndex]);
-    
+}
+
+void uniqueElement(int arr[], int n, int m)
+
+{
+    cout << "{ ";
+
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << "}" << endl;
+}
+
+void intersection(int arr1[], int arr2[], int n, int m)
+{
+    int res[100], resIndex = 0;
+    for (int i = 0; i < n; i++)
+    {
+        bool isDuplicate = false;
+        for (int j = 0; j < resIndex; j++)
+        {
+            if (res[j] == arr1[i])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (isDuplicate)
+            continue;
+
+        for (int k = 0; k < m; k++)
+        {
+            if (arr1[i] == arr2[k])
+            {
+                res[resIndex++] = arr1[i];
+                cout << arr1[i] << " ";
+                break;
+            }
+        }
+    }
 }
 
 int main()
 {
-    int arr[] = {1, 2, 9, 4, 5, 6, 10};
-    int n = sizeof(arr) / sizeof(int);
-    maxMin(arr, n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    int arr1[] = {1, 2, 9, 7, 10, 15};
+    int arr2[] = {1, 2, 7, 9, 4, 5, 6, 10};
+    int n = sizeof(arr1) / sizeof(int);
+    int m = sizeof(arr2) / sizeof(int);
+
+    // maxMin(arr, n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+    // uniqueElement(arr, n);
+
+    intersection(arr1, arr2, n, m);
+
     // sumProduct(arr, n);
     return 0;
 }
